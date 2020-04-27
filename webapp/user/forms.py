9a8bb30e33f690ac059d58948dd38df7aedd3cee
Dark Elephant <holopov@yandex.ru>
2020-04-28 02:08:02 +0300

    update authorization and authentication forms, models and templates

--- webapp/user/forms.py
+++ webapp/user/forms.py
@@ -5,26 +5,25 @@ from webapp.user.models import User
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Имя пользователя', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
+    username = StringField('Имя пользователя', validators=[DataRequired()], render_kw={'class':'validate'})
+    password = PasswordField('Пароль', validators=[DataRequired()], render_kw={'class':'validate'})
     remember_me = BooleanField('Запомнить меня', default=True)
-    submit = SubmitField('Отправить')
+    submit = SubmitField('Отправить', render_kw={'class':'btn waves-effect waves-light'})
 
 
-class RegistrationForm(FlaskForm):
-    username = StringField('Имя пользователя', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    password2 = PasswordField('Повторите пароль',
-                              validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Отправить')
+# class RegistrationForm(FlaskForm):
+#     username = StringField('Имя пользователя', validators=[DataRequired()])
+#     email = StringField('Email', validators=[DataRequired(), Email()])
+#     password = PasswordField('Пароль', validators=[DataRequired()])
+#     password2 = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password')])
+#     submit = SubmitField('Отправить')
 
-    def validate_username(self, username):
-        user = User.query.filter_by(login=username.data).first()
-        if user is not None:
-            raise ValidationError('Пользователь с таким именем уже существует')
+#     def validate_username(self, username):
+#         user = User.query.filter_by(login=username.data).first()
+#         if user is not None:
+#             raise ValidationError('Пользователь с таким именем уже существует')
 
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is not None:
-            raise ValidationError('Пользователь с таким email уже существует')
+    # def validate_email(self, email):
+    #     user = User.query.filter_by(email=email.data).first()
+    #     if user is not None:
+    #         raise ValidationError('Пользователь с таким email уже существует')
